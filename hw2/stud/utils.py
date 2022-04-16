79170aeb99fd740cd735f3b7007dd29d76cc7272
GaspRulez <gasptube@gmail.com>
2022-04-16 18:30:32 +0200

    Add load pretrained embeddings util fn

--- hw2/stud/utils.py
+++ hw2/stud/utils.py
@@ -2,6 +2,36 @@ import os
 
 from transformers import AutoTokenizer
 
+from torchtext.vocab import Vectors
+
+def load_pretrained_embeddings(filename: str, cache_dir: str, vocab: Vocab) -> torch.Tensor:
+    """
+	Loads from a local file static word embedding vectors (e.g. GloVe, FastText)
+	and pairs them with the tokens contain in the given vocabulary.
+
+    Args:
+            filename: name of the file that contains the vectors
+            cache_dir: directory for cached vectors
+            vocab: vocabulary of tokens to be embedded
+    """
+    pretrained_embeddings = Vectors(filename, cache=cache_dir)
+    embeddings = torch.randn(len(vocab), pretrained_embeddings.dim)
+    initialised = 0
+
+    for i, token in enumerate(vocab.get_itos()):
+        token = token.lower()
+        if token in pretrained_embeddings.stoi:
+            initialised += 1
+            embedding = pretrained_embeddings.get_vecs_by_tokens(token)
+            embeddings[i] = embedding
+
+    embeddings[vocab["[PAD]"]] = torch.zeros(pretrained_embeddings.dim)
+
+    print(f"initialised {initialised} embeddings")
+    print(f"randomly initialised {len(vocabulary) - initialised} embeddings")
+
+    return embeddings
+
 
 def get_tokenizer(pretrained_model_name_or_path: str) -> AutoTokenizer:
 	"""
